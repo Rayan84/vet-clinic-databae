396e23a01b9556d8fb41c65c116dab6e05706837
Rayan84 <rayan_84@live.com>
2021-12-09 20:43:26 +0100

    Add find queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND  '2019-01-01';
+SELECT name FROM animals WHERE neutered like true;
+SELECT name FROM animals WHERE neutered = true;
+SELECT name FROM animals WHERE escape_attempts < 3;
