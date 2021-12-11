3cf8a356b98eb85357d92ae162bcd8081d6392da
Rayan84 <rayan_84@live.com>
2021-12-11 02:13:08 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -33,3 +33,19 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT MAX(escape_attempts), neutered FROM animals GROUP BY animals.neutered;
 SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP By animals.species;
 SELECT species, AVG(escape_attempts) from animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY animals.species;
+SELECT * FROM animals JOIN owners ON animals.owners_id = 4;
+SELECT * FROM animals JOIN species ON animals.species_id = 1;
+SELECT animals.name AS animal_name, owners.full_name AS owner_name FROM owners LEFT JOIN animals ON owners.id = animals.owners_id;
+SELECT species.name as species_type , COUNT(*) from animals JOIN species ON species.id = animals.species_id GROUP BY species_type;
+SELECT owners.full_name AS owners_name, animals.name AS animal_name, species.name AS type
+FROM animals
+JOIN species ON species.id = animals.species_id
+JOIN owners ON owners.id = animals.owners_id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+SELECT animals.name AS animal_name FROM animals
+JOIN owners ON owners.id = animals.owners_id
+WHERE animals.escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
+SELECT owners.full_name as owners_name , COUNT(animals.name) as animal_count from owners
+LEFT JOIN animals ON owners.id = animals.owners_id
+GROUP BY owners_name
+ORDER BY animal_count DESC;
